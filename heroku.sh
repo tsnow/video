40e864404d5c1709d62a64383fa8e699fd05f96f
Tim Snowhite <tim@snowhitesolutions.com>
2014-03-09 15:10:34 -0400

    [heroku] script to upload to heroku

---
+++ heroku.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git remote -v heroku 2>/dev/null || git add remote heroku git@heroku.com:tm-video-uploader.git  
+if [[ "$1" == "" ]]; then
+  git push heroku master:master;
+else
+  git push heroku $1:master;
+fi
