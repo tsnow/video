10981f7b0472f3f0fcbc3d675d62bd7a8b7be378
Tim Snowhite <tsnowhite@taximagic.com>
2014-03-13 15:14:04 -0400

    [impressions import] ActiveRecord models for impressions

--- bin/aws_import.rb
+++ bin/aws_import.rb
@@ -1,6 +1,22 @@
 #!/usr/bin/env ruby
 require 'aws-sdk'
-def import_all_impressions(bucket_name)
+require 'active_record'
+class CreatePimAdImpressions < ActiveRecord::Migration
+  def self.up
+    drop_table :pim_ad_impressions if ActiveRecord::Base.connection.table_exists? 'pim_ad_impressions'
+    create_table :pim_ad_impressions do |t|
+      t.integer :pim_id
+      t.integer :content_element_id
+      t.datetime :played_at
+      t.integer :volume
+      t.timestamps
+    end
+  end
+end
+  
+      
+
+class PimAdImpressions < ActiveRecord::Base
 end
 
 class RawImpressions
