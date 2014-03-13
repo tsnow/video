f64ecd16ae958d4d7146c6800b3512d650e5de15
Tim Snowhite <tsnowhite@taximagic.com>
2014-03-13 15:09:09 -0400

    [impressions import] Pull the configuration into a class

--- bin/aws_import.rb
+++ bin/aws_import.rb
@@ -1,12 +1,30 @@
 #!/usr/bin/env ruby
 require 'aws-sdk'
+def import_all_impressions(bucket_name)
+  RawImpressions.new(bucket_name)
+end
 
-AWS.config(
-           :access_key_id => ENV['AWS_ACCESS_KEY'],
-           :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
+class RawImpressions
+  def initialize(bucket_name)
     s3 = AWS::S3.new
-    bucket = s3.buckets['rips-assets-development']
+    bucket = s3.buckets[bucket_name]
     bucket.objects.with_prefix('raw-impressions/').each do |i|
     new_name = i.key.sub('raw-impressions/','archived-impressions/')
     i.move_to(new_name)
   end
+end
+at_exit do
+  if $1 === __FILE__
+
+    required_env = %w(AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_S3_BUCKET)
+
+    fail("Please supply #{required_env.join(', ')}") unless required_env.all?{|i|ENV[i]}
+
+    AWS.config(
+           :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+           :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
+
+    import_all_impressions(ENV['AWS_S3_BUCKET'])
+    
+  end
+end
