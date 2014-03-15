d3178b6285fee4e916140882ec19f67a1430776c
Tim Snowhite <tsnowhite@taximagic.com>
2014-03-14 22:05:36 -0400

    [impressions import] Batch: Something we can use to rollback groups of active record inserts, keep track of errors, and keep counts of items.

--- bin/aws_import.rb
+++ bin/aws_import.rb
@@ -14,7 +14,47 @@ class CreatePimAdImpressions < ActiveRecord::Migration
   end
 end
   
-      
+class Batch < Array
+
+  def self.transaction
+    import = new
+    begin
+      yield import
+    rescue =>e
+      import.rollback
+    end
+    import
+  end
+  def initialize(*args)
+    super(*args)
+    @errored = []
+    @total = []
+  end
+  def push(*args)
+    @total.push(*args)
+    super(*args)
+  end
+  def error(item, errors)
+    @total.push(item)
+    @errored.push(item,errors)
+  end
+  def errors
+    @errored
+  end
+  def total
+    @total
+  end
+  def worked
+    self
+  end
+  def rollback
+    each do |i|
+      next if i.destroyed? || i.new_record?
+      i.destroy
+    end
+    clear
+  end
+end
 
 class PimAdImpressions < ActiveRecord::Base
   def self.store_impressions(impressions)
