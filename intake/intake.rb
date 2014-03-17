b4ba96c96cd2071a38341d4c0812cd49bc93b5ea
Tim Snowhite <tsnowhite@taximagic.com>
2014-03-17 03:22:45 -0400

    [impressions intake]  Add validations on pim_id.

--- intake/intake.rb
+++ intake/intake.rb
@@ -6,6 +6,13 @@ require 'uber-s3'
 require 'uber-s3/connection/em_http_fibered'
 
 class Intake < Goliath::API
+  use Goliath::Rack::Params
+  use Goliath::Rack::DefaultMimeType
+  use Goliath::Rack::Render, 'json'
+
+  use Goliath::Rack::Validation::RequiredParam, {:key => 'pim_id', :type => 'ID'}
+  use Goliath::Rack::Validation::NumericRange, {:key => 'pim_id', :min => 1}
+  use Goliath::Rack::Validation::RequestMethod, %w(POST)           # allow POST requests only  
   
   def response(env)
     bucket = RawImpressions.new(ENV['AWS_S3_BUCKET'])
