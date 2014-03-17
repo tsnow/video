ef7f767b3b7b6f8388b85ba9939b142356dbefc0
Tim Snowhite <tsnowhite@taximagic.com>
2014-03-17 11:32:29 -0400

    [impressions intake import] (refactor: alphabetize gems, whitespace) bin/aws_import.rb

--- bin/aws_import.rb
+++ bin/aws_import.rb
@@ -1,14 +1,11 @@
 #!/usr/bin/env ruby
 require 'active_record'
 require 'aws-sdk'
-require 'multi_json'
 require File.expand_path('../../app/services/import_runner',__FILE__)
-
 require 'logger'
+
+
 AWS.config(:logger => Logger.new($stdout), :log_level => :debug)
-
-
-
 
 at_exit do
   runner = ImportRunner.new
